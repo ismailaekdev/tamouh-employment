ce839a0f0808ba5b4bb5bcf94d21e6fe576f3398
ismailalterweb <ismail.alterweb@gmail.com>
2025-06-12 22:37:23 +0100

    update header, install flowbite and add contact page

---
+++ resources/views/front/contact.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.front')
+
+@section('title', __('Contact Our Team'))
+@section('description', __('Get in touch with our expert team for assistance with buying or selling properties. We are here to help you find the perfect home, office, or investment in Morocco.'))
+@section('og_title', __('Contact Our Team'))
+@section('og_description', __('Get in touch with our expert team for assistance with buying or selling properties. We are here to help you find the perfect home, office, or investment in Morocco.'))
+
+@section('content')
+
+    <h1>
+        {{ __('Contact Our Team') }}
+    </h1>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\ShowContact;
 use App\Http\Controllers\ShowHome;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', ShowHome::class)->name('front.home');
+Route::get('/contact', ShowContact::class)->name('front.contact');
