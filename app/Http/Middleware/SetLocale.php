0991135baae82d9948ed5712c67e4b64179c2600
ismailalterweb <ismail.alterweb@gmail.com>
2025-06-15 13:02:49 +0100

    added contact and maps and bzaaf d l7wayj hh

--- app/Http/Middleware/SetLocale.php
+++ app/Http/Middleware/SetLocale.php
@@ -20,23 +20,19 @@ class SetLocale
      */
     public function handle(Request $request, Closure $next): Response
     {
-        // Check if locale exists in the URL, then set it
         $locale = $request->route('locale');
 
-        // Validate locale value coming from route
         if ($locale !== null && ! in_array($locale, Locale::values())) {
             abort(404);
         }
 
-        // Check if a locale exists in the cookie
         if ($locale === null) {
             $locale = $request->cookie('locale');
         }
 
-        if (in_array($locale, Locale::values())) { // Add your available locales
+        if (in_array($locale, Locale::values())) {
             App::setLocale($locale);
 
-            // Store the locale in a cookie (expiration time is set to 1 year)
             Cookie::queue('locale', $locale, 60 * 24 * 365);
         }
 
