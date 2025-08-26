530764b17986c4bb758f919da60ed0baa332d524
ismailalterweb <ismail.alterweb@gmail.com>
2025-08-26 11:40:41 +0100

    update hero link

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -14,7 +14,7 @@
                 </div>
                 <div class="flex justify-center gap-4">
                     <div class="text-center">
-                        <a class="inline-flex justify-center items-center gap-x-2 text-center bg-primary-700 hover:bg-primary-800 shadow-lg shadow-transparent border border-transparent text-white text-sm font-medium rounded-full focus:outline-hidden py-3 px-6" href="#">
+                        <a href="{{ route('front.contact') }}" class="inline-flex justify-center items-center gap-x-2 text-center bg-primary-700 hover:bg-primary-800 shadow-lg shadow-transparent border border-transparent text-white text-sm font-medium rounded-full focus:outline-hidden py-3 px-6">
                             {{ __('Get Started') }}
                             <svg class="shrink-0 size-4 rtl:rotate-180" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                                 <path d="m9 18 6-6-6-6" />
